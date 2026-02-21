3222436ee456f0ce415a1a32b232dc6e0efd5849
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 17:49:48 +0530

    DSA Revision

--- src/arrays/slidingwindow/CountSubArrays.java
+++ src/arrays/slidingwindow/CountSubArrays.java
@@ -12,7 +12,13 @@ public class CountSubArrays {
 
 //        Brute Force
         for(int i=0; i<arr.length; i++){
-            
+            int sum = 0;
+
+            for(int j=i; j<arr.length; j++){
+                sum+=arr[j];
+                
+            }
+
         }
     }
 }
