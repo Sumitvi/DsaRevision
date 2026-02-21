35a9303ce84644b561a9ac28fa893549b8d52b26
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 17:48:40 +0530

    DSA Revision

--- src/arrays/slidingwindow/CountSubArrays.java
+++ src/arrays/slidingwindow/CountSubArrays.java
@@ -5,5 +5,9 @@ public class CountSubArrays {
     public static void main(String[] args) {
 
 //        Count Subarrays with Given Sum
+
+        int arr[] = {10, 2, -2, -20, 10};
+        int k = -10;
+//        ans is 3
     }
 }
