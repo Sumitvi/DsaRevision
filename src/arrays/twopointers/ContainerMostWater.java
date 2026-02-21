ddff71f58e7aaea03a0c198a03f635a8915a84a5
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 22:35:51 +0530

    Two Pointers

--- src/arrays/twopointers/ContainerMostWater.java
+++ src/arrays/twopointers/ContainerMostWater.java
@@ -10,6 +10,16 @@ public class ContainerMostWater {
         int left = 0 , right = arr.length-1;
         int res = 0;
 
-        
+        while(left < right){
+
+            int water = Math.min(arr[left] , arr[right]) * (right - left);
+            res = Math.max(res , water);
+
+            if(arr[left] < arr[right]){
+                left+=1;
+            }
+
+        }
+
     }
 }
