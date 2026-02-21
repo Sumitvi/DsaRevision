1814c8b11c9738a865bf61b16e9b0759dfd8dca5
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 12:27:34 +0530

    DSAREVISION

--- src/arrays/basic/RemoveDuplicate.java
+++ src/arrays/basic/RemoveDuplicate.java
@@ -1,4 +1,20 @@
 package arrays.basic;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class RemoveDuplicate {
+
+    public static void main(String args[]){
+
+//        Remove Duplicates from Sorted Array
+
+        int arr[] = {0,0,1,1,1,2,2,3,3,4};
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int data : arr){
+            set.add
+        }
+
+    }
 }
