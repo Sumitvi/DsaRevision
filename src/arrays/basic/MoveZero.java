c554343d50d8a7eb7f5f02746467091c3ade00fe
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 12:33:12 +0530

    DSAREVISION

--- src/arrays/basic/MoveZero.java
+++ src/arrays/basic/MoveZero.java
@@ -8,7 +8,14 @@ public class MoveZero {
 //        without 2 pointer using simple logic
 
         int arr[] = {0,1,0,3,12};
-        
+        int count = 0;
+
+        for(int i=0; i<arr.length; i++){
+
+            if(arr[i]!=0){
+                arr[count++] = arr[i];
+            }
+        }
 
 
     }
