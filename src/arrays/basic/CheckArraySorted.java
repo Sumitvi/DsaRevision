dd5316ee0adfd20173c3bb50d19a3d470fd0da05
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 12:14:03 +0530

    DSAREVISION

--- src/arrays/basic/CheckArraySorted.java
+++ src/arrays/basic/CheckArraySorted.java
@@ -4,5 +4,11 @@ public class CheckArraySorted {
     public static void main(String args[]){
 
 //        Check if Array is Sorted
+       int arr[] = [10, 20, 30, 40, 50]
+//        need to return true or false
+
+        boolean assending = true;
+
+       
     }
 }
