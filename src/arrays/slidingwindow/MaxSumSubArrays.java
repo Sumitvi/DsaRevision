c9de05426973d85b11b9d8c0750d0e44a302e49a
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 17:28:20 +0530

    DSA Revision

--- src/arrays/slidingwindow/MaxSumSubArrays.java
+++ src/arrays/slidingwindow/MaxSumSubArrays.java
@@ -3,6 +3,23 @@ package arrays.slidingwindow;
 public class MaxSumSubArrays {
 
     public static void main(String[] args) {
-        
+
+//        Maximum Sum Subarray of Size K
+        int arr [] = {100, 200, 300, 400};
+        int k = 2;
+        int sum = 0;
+        int left = 0;
+        int right = k-1;
+
+        for(int i=0; i<=right; i++){
+            sum+=arr[i];
+        }
+
+        int maxSum = sum;
+
+        while(right < arr.length - 1){
+            
+        }
+
     }
 }
