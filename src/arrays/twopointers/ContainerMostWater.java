296e2496690b5f9bf02cb860ced2c003c23424fc
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 22:34:22 +0530

    Two Pointers

--- src/arrays/twopointers/ContainerMostWater.java
+++ src/arrays/twopointers/ContainerMostWater.java
@@ -7,5 +7,9 @@ public class ContainerMostWater {
 //        Container With Most Water
 
         int arr[] = {1,8,6,2,5,4,8,3,7};
+        int left = 0 , right = arr.length-1;
+        int res = 0;
+
+        
     }
 }
