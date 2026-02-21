204932bb7049aad277aae0a37328161b57e23385
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 12:51:27 +0530

    DSAREVISION

--- src/arrays/slidingwindow/LongestSubarray.java
+++ src/arrays/slidingwindow/LongestSubarray.java
@@ -5,5 +5,23 @@ public class LongestSubarray {
     public static void main(String args[]){
 
 //        Longest Subarray with Sum K (Positive integers)
+        int arr[] = {10, 5, 2, 7, 1, -10};
+        int k = 15;
+
+//        Brute Force
+        int count = 0;
+
+        for(int i=0; i<arr.length; i++){
+            int sum = 0;
+
+            for(int j=i; j<arr.length; j++){
+
+                sum+=arr[j];
+
+                if(sum == k){
+                    count 
+                }
+            }
+        }
     }
 }
