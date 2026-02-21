b08ade45b59c179a96884efdd89587b18ba49914
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 12:32:27 +0530

    DSAREVISION

--- src/arrays/basic/MoveZero.java
+++ src/arrays/basic/MoveZero.java
@@ -4,7 +4,13 @@ public class MoveZero {
 
     public static void main(String[] args) {
 
+//        Move Zeros to End
+//        without 2 pointer using simple logic
+
+        int arr[] = {0,1,0,3,12};
         
+
+
     }
 
 }
