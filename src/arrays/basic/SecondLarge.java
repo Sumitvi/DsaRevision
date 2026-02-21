e1fb16d51da2005c6e7ee91b3cea32f51c42ec1f
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 12:05:44 +0530

    DSAREVISION

---
+++ src/arrays/basic/SecondLarge.java
@@ -0,0 +1,15 @@
+package arrays.basic;
+
+public class SecondLarge {
+
+    public static void main(String args[]){
+
+//        Second Largest Element in Array
+//        to find second largest i need to find max
+//        then if (arr[i]>secondMax && arr[i] < max)
+
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+    }
+}
