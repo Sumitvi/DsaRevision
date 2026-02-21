35cf075f5f242b324d4323e1da8afa8b0c9365e7
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 12:15:49 +0530

    DSAREVISION

--- src/arrays/basic/CheckArraySorted.java
+++ src/arrays/basic/CheckArraySorted.java
@@ -4,16 +4,18 @@ public class CheckArraySorted {
     public static void main(String args[]){
 
 //        Check if Array is Sorted
-       int arr[] = {10, 20, 30, 40, 50};
+       int arr[] = {10, 20, 30, 240, 50};
 //        need to return true or false
 
         boolean assending = true;
 
-       for(int i=0; i<arr.length; i++){
+       for(int i=0; i<arr.length-1; i++){
            if(arr[i] > arr[i+1]){
                assending = false;
            }
        }
+
+        System.out.println(assending);
 
     }
 }
