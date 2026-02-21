f51ada0b8285535789e88b7edb16585b3fa0546e
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 12:06:58 +0530

    DSAREVISION

--- src/arrays/basic/SecondLarge.java
+++ src/arrays/basic/SecondLarge.java
@@ -8,8 +8,17 @@ public class SecondLarge {
 //        to find second largest i need to find max
 //        then if (arr[i]>secondMax && arr[i] < max)
 
+        int arr[] = {2,6,5,8,11};
         int max = Integer.MIN_VALUE;
         int secondMax = Integer.MIN_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        System.out.println(max);
 
     }
 }
