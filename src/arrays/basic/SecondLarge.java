b58d0ac1cce22ebe3bd4ffe8ba037c5f85397375
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 12:07:52 +0530

    DSAREVISION

--- src/arrays/basic/SecondLarge.java
+++ src/arrays/basic/SecondLarge.java
@@ -20,5 +20,13 @@ public class SecondLarge {
 
         System.out.println(max);
 
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > secondMax && arr[i] < max){
+                secondMax = arr[i];
+            }
+        }
+
+        System.out.println(secondMax);
+
     }
 }
