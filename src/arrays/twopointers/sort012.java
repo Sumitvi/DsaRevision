6b0a7ba49d35681197349d404af6d78f2b3a74db
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-21 22:06:08 +0530

    Two Pointers

--- src/arrays/twopointers/sort012.java
+++ src/arrays/twopointers/sort012.java
@@ -1,4 +1,20 @@
 package arrays.twopointers;
 
+import java.util.Arrays;
+
 public class sort012 {
+
+    public static void main(String[] args) {
+
+//        sort 0 , 1 , 2
+        int arr[] = {0, 1, 2, 0, 1, 2};
+
+//        Complexity - O(n lon n)
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+//        Better Approach
+
+
+    }
 }
