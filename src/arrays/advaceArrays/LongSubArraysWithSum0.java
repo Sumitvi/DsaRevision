5825fa4f17e94aef5afb397c617ed4b3e7d2be9e
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 12:01:42 +0530

    Arrays Revision

--- src/arrays/advaceArrays/LongSubArraysWithSum0.java
+++ src/arrays/advaceArrays/LongSubArraysWithSum0.java
@@ -5,21 +5,34 @@ public class LongSubArraysWithSum0 {
     public static void main(String[] args) {
 
 //        longest Subarrays with sum equals to zero
+//        BRUTE FORCE
+//        int arr[] = {15, -2, 2, -8, 1, 7, 10};
+//        int maxLen = 0;
+//
+//        for(int i=0; i<arr.length; i++){
+//            int sum = 0;
+//
+//            for (int j=i; j<arr.length; j++){
+//                sum+=arr[j];
+//
+//                if(sum == 0){
+//                    maxLen = Math.max(maxLen , j-i+1);
+//                }
+//            }
+//        }
+//
+//        System.out.println("Max Subarrays with sum 0 is : "+ maxLen);
+
+//        OPTIMAL USING PREFIX SUM + HASHMAP
         int arr[] = {15, -2, 2, -8, 1, 7, 10};
+        int prefixSum = 0;
         int maxLen = 0;
 
         for(int i=0; i<arr.length; i++){
-            int sum = 0;
-
-            for (int j=i; j<arr.length; j++){
-                sum+=arr[j];
-
-                if(sum == 0){
-                    maxLen = Math.max(maxLen , j-i+1);
-                }
-            }
+            
         }
 
-        System.out.println("Max Subarrays with sum 0 is : "+ maxLen);
+
+
     }
 }
