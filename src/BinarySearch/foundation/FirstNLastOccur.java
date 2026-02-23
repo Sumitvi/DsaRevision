6c75f78e7892bb2b93ef4f772ff4a1e2f9c5bf4b
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 13:43:38 +0530

    Binary Search

---
+++ src/BinarySearch/foundation/FirstNLastOccur.java
@@ -0,0 +1,11 @@
+package BinarySearch.foundation;
+
+public class FirstNLastOccur {
+    public static void main(String[] args) {
+
+//        Print First and Last Occurance
+        int arr[] = {3, 4, 13, 13, 13, 20, 40};
+        int target = 13;
+        
+    }
+}
