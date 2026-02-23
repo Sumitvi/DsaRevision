c7cb40228301f96e9059961e59ac20dc48dda96e
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 12:50:08 +0530

    Binary Search

---
+++ src/BinarySearch/foundation/SearchX.java
@@ -0,0 +1,24 @@
+package BinarySearch.foundation;
+
+public class SearchX {
+    public static void main(String[] args) {
+
+//        Search X in Sorted Array
+//        Binary Search Works only on Sorted Arrays
+        int arr[] = {-1,0,3,5,9,12};
+        int target = 9;
+        int start = 0 , end = arr.length-1;
+
+        while(start <= end){
+
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] == target){
+                System.out.println(mid);
+            }else if()
+        }
+
+
+
+    }
+}
