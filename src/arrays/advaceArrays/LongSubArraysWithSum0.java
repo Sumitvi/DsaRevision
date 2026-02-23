3dd516350c7148b62d23c9c943a588b8ad0b6841
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 11:48:17 +0530

    Arrays Revision

---
+++ src/arrays/advaceArrays/LongSubArraysWithSum0.java
@@ -0,0 +1,15 @@
+package arrays.advaceArrays;
+
+public class LongSubArraysWithSum0 {
+
+    public static void main(String[] args) {
+
+//        longest Subarrays with sum equals to zero
+        int arr[] = {15, -2, 2, -8, 1, 7, 10};
+        int maxLen = 0;
+
+        for(int i=0; i<arr.length; i++){
+            
+        }
+    }
+}
