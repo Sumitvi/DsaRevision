1cd9712e8ffc38ca14da5180e7738738f1610681
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 22:25:48 +0530

    Binary Search Medium Questions

--- src/BinarySearch/medium/KokoEating.java
+++ src/BinarySearch/medium/KokoEating.java
@@ -2,6 +2,37 @@ package BinarySearch.medium;
 
 public class KokoEating {
     public static void main(String[] args) {
+
         
+
+
+    }
+}
+
+//Brute Force
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int max = Integer.MIN_VALUE;
+
+        for(int pile : piles){
+            max = Math.max(max , pile);
+        }
+
+        for(int k=1; k<=max; k++){
+            int hours = 0;
+
+            for(int pile : piles){
+                hours+=(pile + k - 1)/k;
+            }
+
+            if(hours <= h){
+                return k;
+            }
+        }
+
+        return -1;
+
+
     }
 }
