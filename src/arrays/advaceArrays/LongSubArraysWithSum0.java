3c65e17ed6381ac26e891d7ef7fad2f6055fad3a
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 11:49:55 +0530

    Arrays Revision

--- src/arrays/advaceArrays/LongSubArraysWithSum0.java
+++ src/arrays/advaceArrays/LongSubArraysWithSum0.java
@@ -12,6 +12,10 @@ public class LongSubArraysWithSum0 {
         for(int i=0; i<arr.length; i++){
             for (int j=i; j<arr.length; j++){
                 sum+=arr[j];
+
+                if(sum == 0){
+                    maxLen = Math.max(maxLen , j-i+1);
+                }
             }
         }
     }
