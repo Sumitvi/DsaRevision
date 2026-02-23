ef696e3ba8771484e01c2ab6d8b10fe55514fb8b
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 17:40:33 +0530

    BS

--- src/BinarySearch/medium/SearchInRotatedSorted.java
+++ src/BinarySearch/medium/SearchInRotatedSorted.java
@@ -3,6 +3,16 @@ package BinarySearch.medium;
 public class SearchInRotatedSorted {
     public static void main(String[] args) {
 
+//
+        int arr[] = {1,1,2,3,3,4,4,8,8};
+//      i can do using HashMap by storing freq if ele found freq 1 then it is a ans
+
+        for(int i=0; i<arr.length; i++){
+            
+        }
+
+
+
 
     }
 }
