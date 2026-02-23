4327e6001740336964ee6711d999c2128d24dd0e
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 13:32:46 +0530

    Binary Search

--- src/BinarySearch/foundation/LowerBound.java
+++ src/BinarySearch/foundation/LowerBound.java
@@ -11,10 +11,20 @@ public class LowerBound {
 //        smaller than the target, the lower bound will be the length of the array.
         int arr[] = {2, 3, 7, 10, 11, 11, 25};
         int target = 9;
+        boolean found = false;
 
 //        using linear Search
         for(int i=0; i<arr.length; i++){
-            
+            if(arr[i] >= target){
+                System.out.println("Lower Bound Index : "+ i);
+                found = false;
+                break;
+            }
+        }
+
+
+        if(!found){
+            System.out.println("The Length of Array : "+arr.length);
         }
     }
 }
