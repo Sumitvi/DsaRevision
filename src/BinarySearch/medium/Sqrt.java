54e03ac98118c9aea57a06fe83980bcfa6580278
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 21:53:34 +0530

    Binary Search Medium Questions

--- src/BinarySearch/medium/Sqrt.java
+++ src/BinarySearch/medium/Sqrt.java
@@ -4,5 +4,9 @@ public class Sqrt {
     public static void main(String[] args) {
 
 //        Find Square Root of a Number
+        int x = 4;
+        int start = 0;
+        int end = x;
+        int ans = 0;
     }
 }
