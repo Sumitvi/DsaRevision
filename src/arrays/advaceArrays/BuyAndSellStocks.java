079357f7bd6c1246af1fbac95e50792abdb16ee7
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 12:33:32 +0530

    Arrays Revision

---
+++ src/arrays/advaceArrays/BuyAndSellStocks.java
@@ -0,0 +1,18 @@
+package arrays.advaceArrays;
+
+public class BuyAndSellStocks {
+    public static void main(String[] args) {
+
+//        Stock Buy and Sell
+//         Best Time to Buy and Sell Stock
+//        Buy at min Price
+//        Sell at maxPrice
+        int prices[] = {7,1,5,3,6,4};
+        int minPri = Integer.MIN_VALUE;
+        int maxPri = 0;
+
+        
+
+
+    }
+}
