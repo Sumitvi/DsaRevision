a75f46127e37994624a0f3ca00715ddbfac76f20
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 14:12:32 +0530

    Binary Search

--- src/BinarySearch/foundation/OccurOfEle.java
+++ src/BinarySearch/foundation/OccurOfEle.java
@@ -1,4 +1,20 @@
 package BinarySearch.foundation;
 
 public class OccurOfEle {
+
+    public static void main(String[] args) {
+
+//        Count Occurrences in Sorted Array
+        int arr[] = {2, 2 , 3 , 3 , 3 , 3 , 4};
+        int x = 3;
+        int count = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==x){
+                count++;
+            }
+        }
+
+        System.out.println();
+    }
 }
