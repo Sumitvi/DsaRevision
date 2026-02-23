3e920ec058e5110e62ea648f15549cd3b722cea6
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-23 13:27:23 +0530

    Binary Search

---
+++ src/BinarySearch/foundation/LowerBound.java
@@ -0,0 +1,20 @@
+package BinarySearch.foundation;
+
+public class LowerBound {
+    public static void main(String[] args) {
+
+//        Given a sorted array arr[] and a number target,
+//        find the lower bound of the target in this given array.
+//        The lower bound of a number is defined as the smallest index
+//        in the sorted array where the element is greater than or equal to the target.
+//        Note: If all the elements in the given array are
+//        smaller than the target, the lower bound will be the length of the array.
+        int arr[] = {2, 3, 7, 10, 11, 11, 25};
+        int target = 9;
+
+//        using linear Search
+        for(int i=0; i<arr.length; i++){
+            
+        }
+    }
+}
