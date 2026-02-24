ae5ce61f0ad79dab42c1107398dd6185d76e6277
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-24 12:34:03 +0530

    String Easy

--- src/String/easy/Anagrams.java
+++ src/String/easy/Anagrams.java
@@ -1,5 +1,7 @@
 package String.easy;
 
+import java.util.Arrays;
+
 public class Anagrams {
     public static void main(String[] args) {
 
@@ -10,7 +12,13 @@ public class Anagrams {
         same frequency. So, they are anagrams.*/
 
         char ch1[] = s1.toCharArray();
+        char ch2[] = s2.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
         
+
 
 
     }
