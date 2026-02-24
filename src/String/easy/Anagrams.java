488355a998932567e8750c9f06d37a812c7706f2
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-24 12:28:09 +0530

    String Easy

--- src/String/easy/Anagrams.java
+++ src/String/easy/Anagrams.java
@@ -1,4 +1,13 @@
 package String.easy;
 
 public class Anagrams {
+    public static void main(String[] args) {
+
+        /*Input: s1 = “geeks”  s2 = “kseeg”
+        Output: true
+        Explanation: Both the string have same characters with
+        same frequency. So, they are anagrams.*/
+
+        
+    }
 }
