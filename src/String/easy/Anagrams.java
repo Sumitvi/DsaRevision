6994b7d7421945ad039e1b14e695a293c6504796
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-24 12:36:07 +0530

    String Easy

--- src/String/easy/Anagrams.java
+++ src/String/easy/Anagrams.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 public class Anagrams {
     public static void main(String[] args) {
 
-        String s1 = "geek";
+        String s1 = "geeks";
         String s2 = "kseeg";
+
+        if(s1.length()!=s2.length()){
+            System.out.println("False");
+        }
         /*Output: true
         Explanation: Both the string have same characters with
         same frequency. So, they are anagrams.*/
@@ -17,7 +21,7 @@ public class Anagrams {
         Arrays.sort(ch1);
         Arrays.sort(ch2);
 
-        
+        System.out.println(Arrays.compare(ch1 , ch2));
 
 
 
