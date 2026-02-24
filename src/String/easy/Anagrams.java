512d5b68495aab04fbc8c9d9b99ac2c928bc75ea
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-24 13:00:14 +0530

    String Easy

--- src/String/easy/Anagrams.java
+++ src/String/easy/Anagrams.java
@@ -7,12 +7,9 @@ import java.util.HashMap;
 public class Anagrams {
     public static void main(String[] args) {
 
-        String s1 = "allergy";
-        String s2 = "allergyy";
+        String s1 = "geeks";
+        String s2 = "kseeg";
 
-        if(s1.length()!=s2.length()){
-            System.out.println("False");
-        }
         /*Output: true
         Explanation: Both the string have same characters with
         same frequency. So, they are anagrams.*/
@@ -28,6 +25,7 @@ public class Anagrams {
 
 //        Using HashMap
 
+        System.out.println(IsAnagram(s1 , s2));
 
 
 
---
+++ src/String/easy/LongestCommonPrefix.java
@@ -0,0 +1,14 @@
+package String.easy;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+
+//        Longest Common Prefix
+        String strs[] = {"flower","flow","flight"};
+        Arrays.sort(strs);
+        System.out.println(Arrays.toString(strs));
+    }
+}
