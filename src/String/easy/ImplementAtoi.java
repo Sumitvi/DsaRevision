90f3383910f05dd4cc524c6c911413c9566c040d
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-25 13:17:40 +0530

    String Easy Questions Revision

---
+++ src/String/easy/ImplementAtoi.java
@@ -0,0 +1,17 @@
+package String.easy;
+
+public class ImplementAtoi {
+    public static void main(String[] args) {
+
+
+    }
+
+    static int StringToInteger(String s){
+
+        s = s.trim();
+
+        if(s.isEmpty()){
+            return 0;
+        }
+    }
+}
