c81760186d57ce657770fdda11281d08bbaff3ed
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-25 13:18:33 +0530

    String Easy Questions Revision

--- src/String/easy/ImplementAtoi.java
+++ src/String/easy/ImplementAtoi.java
@@ -9,9 +9,16 @@ public class ImplementAtoi {
     static int StringToInteger(String s){
 
         s = s.trim();
+        int i=0;
+        int sign = 1;
 
         if(s.isEmpty()){
             return 0;
         }
+
+        if(s.charAt(i)=='-' || s.charAt(i)=='+'){
+            
+        }
+
     }
 }
