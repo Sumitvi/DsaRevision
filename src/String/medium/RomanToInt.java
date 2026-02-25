31802b248433f8c77750d1f7a3efbee53df8c199
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-25 14:14:23 +0530

    String Medium Questions Revision

--- src/String/medium/RomanToInt.java
+++ src/String/medium/RomanToInt.java
@@ -4,11 +4,30 @@ public class RomanToInt {
     public static void main(String[] args) {
 
 //         Roman to Integer
+        String s = "LVIII";
+        System.out.println(RomanToInt(s));
 
     }
 
     static int getVal(char ch){
-        
+
+        switch (ch){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return 0;
     }
 
 
