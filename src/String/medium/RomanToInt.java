c738583956fba2b611c8a9fd3f191fe85d07a22f
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-25 14:08:17 +0530

    String Medium Questions Revision

--- src/String/medium/RomanToInt.java
+++ src/String/medium/RomanToInt.java
@@ -4,6 +4,21 @@ public class RomanToInt {
     public static void main(String[] args) {
 
 //         Roman to Integer
-        
+
     }
+
+
+
+    static int RomToInt(String s){
+
+        int n = s.length();
+        int sum = 0;
+
+        for(int i=0; i<n; i++){
+            
+        }
+
+    }
+
+
 }
