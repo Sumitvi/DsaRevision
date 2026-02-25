0f8adf2a28358661ff951f3f502c6a63ff888bf6
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-25 12:44:27 +0530

    String Easy Questions Revision

--- src/String/easy/RotateString.java
+++ src/String/easy/RotateString.java
@@ -4,6 +4,13 @@ public class RotateString {
     public static void main(String[] args) {
 
 //        Rotate String
+        String s = "Sumit";
+
+    }
+
+    static String StringRotate(String s){
+//        using String Builder
+        StringBuilder sb = new StringBuilder();
         
     }
 }
