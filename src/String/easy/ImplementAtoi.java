2bd6e89467ca6c8939f3b4989e4ffed2546553e0
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-25 13:20:51 +0530

    String Easy Questions Revision

--- src/String/easy/ImplementAtoi.java
+++ src/String/easy/ImplementAtoi.java
@@ -11,14 +11,27 @@ public class ImplementAtoi {
         s = s.trim();
         int i=0;
         int sign = 1;
+        int n = s.length();
+        long num = 0;
 
         if(s.isEmpty()){
             return 0;
         }
 
         if(s.charAt(i)=='-' || s.charAt(i)=='+'){
-            
+            sign = (s.charAt(i) == '-')?-1:1;
+            i++;
         }
+
+        while(i < n && Character.isDigit(s.charAt(i))){
+            num = num * 10 + (s.charAt(i) - '0');
+
+            if(num * sign > Integer.MAX_VALUE){
+                
+            }
+        }
+
+
 
     }
 }
