981c8d313936b1d75996fddb58f8e838c2a0b9b8
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-26 11:55:07 +0530

    String Medium Questions

--- src/String/medium/SortByFreq.java
+++ src/String/medium/SortByFreq.java
@@ -1,4 +1,20 @@
 package String.medium;
 
+import java.util.HashMap;
+
 public class SortByFreq {
+    public static void main(String[] args) {
+
+
+    }
+    static String frequencySort(String s) {
+        // code here
+
+        // storing in hashMap by managing freq
+        HashMap<Character , Integer > map = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch , map.getOrDefault(ch , 0) + 1);
+        }
 }
