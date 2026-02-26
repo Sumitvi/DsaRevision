56150aeb47f586c0615fcf6b984ee94cccca321c
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-26 12:57:00 +0530

    String Medium Questions

--- src/String/medium/LongestPalindromeSubstring.java
+++ src/String/medium/LongestPalindromeSubstring.java
@@ -9,7 +9,16 @@ public class LongestPalindromeSubstring {
     static boolean isPalindrome(String s , int i , int j){
 
         while(i < j){
-            
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+
+            if(ch1 != ch2){
+                return false;
+            }
+            i++;
+            j--;
         }
+
+        return true;
     }
 }
