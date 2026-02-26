73a38a74ef1bcfaffade07d03e432f1e51f0fe55
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-26 12:34:22 +0530

    String Medium Questions

--- src/String/medium/LargestOddNum.java
+++ src/String/medium/LargestOddNum.java
@@ -12,7 +12,7 @@ public class LargestOddNum {
         int n = s.length();
 
         for(int i=n-1; i>=0; i--){
-            if(s.charAt(i)!=0){
+            if(s.charAt(i) % 2 !=0){
                 return s.substring(0 , i+1);
             }
         }
