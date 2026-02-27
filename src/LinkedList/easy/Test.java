d534f3636b53d36485eb6c9e045e060470c8d371
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-27 12:01:44 +0530

    Easy Que of ArrayList

--- src/LinkedList/easy/Test.java
+++ src/LinkedList/easy/Test.java
@@ -1,8 +1,19 @@
 package LinkedList.easy;
 
+import java.util.LinkedList;
+
 public class Test {
     public static void main(String[] args) {
 
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+
+        System.out.println(ll);
+        System.out.println(ll.size());
         
     }
 }
