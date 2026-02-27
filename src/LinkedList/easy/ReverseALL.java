a164a4824abc7af02eb48d334e92b91d3e999de4
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-27 16:52:41 +0530

    Linkedlist Easy Question

--- src/LinkedList/easy/ReverseALL.java
+++ src/LinkedList/easy/ReverseALL.java
@@ -1,8 +1,39 @@
 package LinkedList.easy;
 
 public class ReverseALL {
+
+    static class Node {
+        int data;
+        MiddleNodeofLL.Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static MiddleNodeofLL.Node head;
+
+
+    static void add(int data) {
+        MiddleNodeofLL.Node newNode = new MiddleNodeofLL.Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        MiddleNodeofLL.Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
     public static void main(String[] args) {
 
 //         Reverse a LinkedList (Iterative)
+
+        
     }
 }
