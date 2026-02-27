2d0394f7851b8b8688ca81d1e093f04dc7fcd81a
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-27 16:27:01 +0530

    Linkedlist Easy Question

--- src/LinkedList/easy/MiddleNodeofLL.java
+++ src/LinkedList/easy/MiddleNodeofLL.java
@@ -6,6 +6,13 @@ public class MiddleNodeofLL {
     public static void main(String[] args) {
 
         LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+
+        
 
 
     }
