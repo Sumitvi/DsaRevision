de4f2ebd8d6213f841f3c2d2110fbbc96c6d07d6
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-27 16:33:29 +0530

    Linkedlist Easy Question

--- src/LinkedList/easy/MiddleNodeofLL.java
+++ src/LinkedList/easy/MiddleNodeofLL.java
@@ -1,47 +1,51 @@
 package LinkedList.easy;
-import java.util.LinkedList;
-
-
-
-
 
 public class MiddleNodeofLL {
 
-
-    class Node{
-
-        Node next;
+    static class Node {
         int data;
+        Node next;
 
-        public Node(int data , Node next){
+        Node(int data) {
             this.data = data;
             this.next = null;
         }
-
-
-
     }
 
-    public static Node head;
-    public static LlImplenetation.Node tail;
+    static Node head;
+
+    // Method to add node at end
+    static void add(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
 
     public static void main(String[] args) {
 
-        LinkedList<Integer> ll = new LinkedList<>();
-        ll.add(1);
-        ll.add(2);
-        ll.add(3);
-        ll.add(4);
-        ll.add(5);
+        add(1);
+        add(2);
+        add(3);
+        add(4);
+        add(5);
 
         Node slow = head;
         Node fast = head;
 
-        while(fast!=null && fast.next!=null){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
 
-
+        System.out.println("Middle Node: " + slow.data);
     }
 }
