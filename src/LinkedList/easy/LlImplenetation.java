7ce19b059c3aa384b0fc81f05856d18cfbaf8e5a
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-27 12:28:51 +0530

    Easy Que of ArrayList

---
+++ src/LinkedList/easy/LlImplenetation.java
@@ -0,0 +1,24 @@
+package LinkedList.easy;
+
+public class LlImplenetation {
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node (int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    
+
+
+    public static void main(String[] args) {
+
+    }
+}
