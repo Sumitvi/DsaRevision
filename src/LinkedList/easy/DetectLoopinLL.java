8583a15229b3261d9e343e8d76614b4db95b8117
Sumit Vishwakarma <sv0042595@gmail.com>
2026-02-27 21:37:13 +0530

    LinkedList Questions

--- src/LinkedList/easy/DetectLoopinLL.java
+++ src/LinkedList/easy/DetectLoopinLL.java
@@ -2,8 +2,28 @@ package LinkedList.easy;
 
 public class DetectLoopinLL {
 
+    public boolean detectLoop(MiddleNodeofLL.Node head) {
+        // code here
+
+        MiddleNodeofLL.Node slow = head;
+        MiddleNodeofLL.Node fast = head;
+
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(slow == fast){
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
 
-        
+
+
     }
 }
