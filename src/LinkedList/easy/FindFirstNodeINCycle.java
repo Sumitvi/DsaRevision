67030a66ff38cbc3e7dc4f354c5d5c852103592a
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-02 11:40:39 +0530

    LinkedList Questions

--- src/LinkedList/easy/FindFirstNodeINCycle.java
+++ src/LinkedList/easy/FindFirstNodeINCycle.java
@@ -1,10 +1,39 @@
 package LinkedList.easy;
 
 public class FindFirstNodeINCycle {
+
+    static int cycleStart(LlImplenetation.Node head) {
+        // code here
+
+        LlImplenetation.Node slow = head;
+        LlImplenetation.Node fast = head;
+
+        while(fast!=null && fast.next!=null){
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+
+                slow = head;
+
+                while(slow!=fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+
+                return slow.data;
+            }
+        }
+
+        return -1;
+
+    }
+
     public static void main(String[] args) {
 
 //        find the first node in cycle
 
-        
+
     }
 }
