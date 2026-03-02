c44e021858054dab1e02d062612c94bd1360cb45
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-02 12:08:45 +0530

    LinkedList Questions

--- src/LinkedList/easy/PointofIntersection.java
+++ src/LinkedList/easy/PointofIntersection.java
@@ -19,6 +19,10 @@ public class PointofIntersection {
             }else{
                 ptr1 = head2;
             }
+
+            if(ptr2!=null){
+                ptr2 = ptr2.next;
+            }
         }
 
 
