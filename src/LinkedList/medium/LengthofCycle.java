bd6f952144b8580db20a690dd749da2276817afc
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-02 13:58:33 +0530

    LinkedList Questions

--- src/LinkedList/medium/LengthofCycle.java
+++ src/LinkedList/medium/LengthofCycle.java
@@ -1,9 +1,21 @@
 package LinkedList.medium;
 
 public class LengthofCycle {
-    public static void main(String[] args) {
+
+    static int lengthOfLoop(LLImplenetation.Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast!=null && fast.next!=null){
+            
+        }
+    }
+
+
+
+        public static void main(String[] args) {
 
 //        find the length of cycle
-        
+
     }
 }
