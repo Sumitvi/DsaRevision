dcb1e44de40d32c7b327fd2cee4fb64c90776bfc
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-02 11:53:50 +0530

    LinkedList Questions

--- src/LinkedList/easy/CheckPalindromeLL.java
+++ src/LinkedList/easy/CheckPalindromeLL.java
@@ -1,6 +1,19 @@
 package LinkedList.easy;
 
+import java.util.Stack;
+
 public class CheckPalindromeLL {
+
+    public boolean isPalindrome(LlImplenetation.Node head) {
+
+        // using stack Approach
+        LlImplenetation.Node currNode = head;
+        Stack<Integer> st = new Stack<>();
+
+
+    }
+
+
     public static void main(String[] args) {
 
 //        Check if a LL is Plaindrome or not
