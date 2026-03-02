eb2121e2dcaccd661578773f72f554817e8f042b
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-02 13:28:21 +0530

    LinkedList Questions

--- src/LinkedList/medium/AddTwoNumbers.java
+++ src/LinkedList/medium/AddTwoNumbers.java
@@ -9,7 +9,17 @@ public class AddTwoNumbers {
         int carry = 0;
 
         while(l1!=null && l2!=null){
-            
+            int sum = 0 + carry;
+
+            if(l1!=null){
+                sum+=l1.data;
+                l1 = l1.next;
+            }
+
+            if(l2!=null){
+                sum+=l2.data;
+                l2 = l2.next;
+            }
         }
 
     }
