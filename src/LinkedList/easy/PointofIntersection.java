3f29584b50fa945149a23b6f49de365ddb8e3e95
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-02 12:06:03 +0530

    LinkedList Questions

---
+++ src/LinkedList/easy/PointofIntersection.java
@@ -0,0 +1,15 @@
+package LinkedList.easy;
+
+public class PointofIntersection {
+
+    public LlImplenetation.Node intersectPoint(LlImplenetation.Node head1, LlImplenetation.Node head2) {
+        // code here
+        
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
