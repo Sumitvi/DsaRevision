58b73fb80e4ab196e43392edd43f543c0abfca7a
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-03 12:03:26 +0530

    LinkedList Medium Questions

--- src/LinkedList/medium/ReverseLinkedListofKNodes.java
+++ src/LinkedList/medium/ReverseLinkedListofKNodes.java
@@ -3,8 +3,13 @@ package LinkedList.medium;
 public class ReverseLinkedListofKNodes {
 
     static LLImplenetation.Node reverseKGroup(LLImplenetation.Node head , int k){
+        LLImplenetation.Node temp = head;
+        int count = 0;
 
-        
+        while (temp!=null && count < k){
+
+        }
+
 
 
     }
