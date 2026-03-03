73c29b73f75a73b0d5219a622c8ab917a5a3fb53
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-03 12:02:38 +0530

    LinkedList Medium Questions

--- src/LinkedList/medium/ReverseLinkedListofKNodes.java
+++ src/LinkedList/medium/ReverseLinkedListofKNodes.java
@@ -1,6 +1,15 @@
 package LinkedList.medium;
 
 public class ReverseLinkedListofKNodes {
+
+    static LLImplenetation.Node reverseKGroup(LLImplenetation.Node head , int k){
+
+        
+
+
+    }
+
+
     public static void main(String[] args) {
 
 //        Reverse k node of a LL
