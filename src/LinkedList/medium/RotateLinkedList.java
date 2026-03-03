995b73392cb846f28004cb68a6ff7e8652addbc8
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-03 11:30:27 +0530

    LinkedList Medium Questions

--- src/LinkedList/medium/RotateLinkedList.java
+++ src/LinkedList/medium/RotateLinkedList.java
@@ -3,6 +3,14 @@ package LinkedList.medium;
 public class RotateLinkedList {
 
     public LLImplenetation.Node rotate(LLImplenetation.Node head, int k) {
+
+        if(k==0 || head == null){
+            return head;
+        }
+
+        LLImplenetation.Node curr = head;
+        int len = 0;
+
         
     }
         public static void main(String[] args) {
