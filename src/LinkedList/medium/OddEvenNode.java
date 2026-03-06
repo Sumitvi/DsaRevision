b855e478c0e2540acb467ef89a636755c0e132f5
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-06 12:35:36 +0530

    LinkedList Questions

---
+++ src/LinkedList/medium/OddEvenNode.java
@@ -0,0 +1,13 @@
+package LinkedList.medium;
+
+public class OddEvenNode {
+
+    static LLImplenetation.Node SegrigateOddEven(LLImplenetation.Node head){
+        
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
