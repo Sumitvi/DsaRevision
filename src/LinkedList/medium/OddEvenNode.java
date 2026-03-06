6096b3e2ef7e5794834ba39db11aaf8133022466
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-06 12:36:25 +0530

    LinkedList Questions

--- src/LinkedList/medium/OddEvenNode.java
+++ src/LinkedList/medium/OddEvenNode.java
@@ -3,7 +3,14 @@ package LinkedList.medium;
 public class OddEvenNode {
 
     static LLImplenetation.Node SegrigateOddEven(LLImplenetation.Node head){
-        
+
+        if(head == null){
+            return head;
+        }
+
+        LLImplenetation.Node odd = head;
+        LLImplenetation.Node even = head.next;
+
     }
 
     public static void main(String[] args) {
