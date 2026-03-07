2db2869ca19bbead1ad0e7dbf197c559d6edc563
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-07 12:50:24 +0530

    2D Matrix Questions

--- src/twoDArrays/ImplementationPractice.java
+++ src/twoDArrays/ImplementationPractice.java
@@ -8,7 +8,11 @@ public class ImplementationPractice {
         int arr[][] = new int[2][2];
         Scanner sc = new Scanner(System.in);
 
-        
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                
+            }
+        }
 
     }
 }
