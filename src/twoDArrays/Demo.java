1e65f05e7ba13b00c09c710e8e5e4b38c25306b1
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-07 12:47:15 +0530

    2D Matrix Questions

--- src/twoDArrays/Demo.java
+++ src/twoDArrays/Demo.java
@@ -11,6 +11,7 @@ public class Demo {
             for(int j=0; j<arr[0].length; j++){
                 System.out.print(arr[i][j] + " ");
             }
+            System.out.println();
         }
 
 
