b33b3511bf492f6cb9614676937e598d5155667e
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-07 13:06:11 +0530

    2D Matrix Questions

--- src/twoDArrays/MedianInRowWiseSorted.java
+++ src/twoDArrays/MedianInRowWiseSorted.java
@@ -4,6 +4,16 @@ public class MedianInRowWiseSorted {
 
     public static void main(String[] args) {
 
-        
+
+
+    }
+
+
+    static int median(int arr[][]){
+
+        for(int i=0;i<arr.length; i++){
+
+            
+        }
     }
 }
