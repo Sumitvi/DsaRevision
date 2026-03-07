7f15a12dfae94c80eee603a08944ff68db5d1af7
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-07 12:44:34 +0530

    2D Matrix Questions

--- src/twoDArrays/Demo.java
+++ src/twoDArrays/Demo.java
@@ -4,6 +4,14 @@ public class Demo {
     public static void main(String[] args) {
 
         int arr[][] = {{1 , 2 , 3} , {1 , 2 , 3} , {1 , 2 , 3}};
-        
+
+//        printing the matrix
+
+        for(int i=0; i<arr.length;i++){
+            for(int j=0; j<arr[0].length; j++){
+                System.out.println(arr[i][j]);
+            }
+        }
+
     }
 }
