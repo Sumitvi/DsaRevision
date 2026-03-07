bb4662ba0368b61fc07db5bf356b69f062a34f2f
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-07 12:57:59 +0530

    2D Matrix Questions

--- src/twoDArrays/ImplementationPractice.java
+++ src/twoDArrays/ImplementationPractice.java
@@ -16,5 +16,16 @@ public class ImplementationPractice {
             }
         }
 
+
+        System.out.println("Matrix is ");
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+
     }
 }
