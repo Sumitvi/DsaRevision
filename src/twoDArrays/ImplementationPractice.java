b323201915680546093bfdb29d4fdb045087cb52
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-07 12:52:24 +0530

    2D Matrix Questions

--- src/twoDArrays/ImplementationPractice.java
+++ src/twoDArrays/ImplementationPractice.java
@@ -8,7 +8,8 @@ public class ImplementationPractice {
         int arr[][] = new int[2][2];
         Scanner sc = new Scanner(System.in);
 
-//        Enter Arrays Elements
+
+        System.out.println("Enter Arrays Elements");
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[0].length; j++){
                 arr[i][j] = sc.nextInt();
